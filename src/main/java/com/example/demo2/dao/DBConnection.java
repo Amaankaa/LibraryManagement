d34010270adbf8073c16e85e334d9f7a1599a98e
siham <sihamkassim735@gmail.com>
2024-12-26 00:42:27 +0300

    Added DBConnection

--- src/main/java/com/example/demo2/dao/DBConnection.java
+++ src/main/java/com/example/demo2/dao/DBConnection.java
@@ -1,4 +1,23 @@
 package com.example.demo2.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError("Failed to load MySQL driver: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
