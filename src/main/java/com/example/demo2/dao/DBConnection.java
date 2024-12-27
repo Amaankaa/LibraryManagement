970a355d7fb9f67ea991a89473ba019c6f83836d
bettytracey27 <bettyder2002@gmail.com>
2024-12-27 08:01:08 -0800

    Deleted 'HelloServlet'

--- src/main/java/com/example/demo2/dao/DBConnection.java
+++ src/main/java/com/example/demo2/dao/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "root123";
+    private static final String PASSWORD = "27@mysql";
 
     static {
         try {
